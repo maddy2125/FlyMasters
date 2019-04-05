aa05551f93164befd0f9ff145e826314d0fb56cb
Madhu Babu <m.madhu.2125@gmail.com>
2019-04-05 21:24:41 +0530

    It was ignored in prev check-in

---
+++ FlyMasters.API/Models/ProfileImportModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FlyMasters.API.ViewModels
+{
+    public class ProfileImportModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }    
+}
